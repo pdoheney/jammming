6ac280605e289b71f4a680b4efd247e22ab416cf
Paul <pdoheney1@gmail.com>
2023-11-06 14:08:20 -0500

    Add search result logic and basic css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import SearchBar from './Components/SearchBar/SearchBar';
+import SearchResults from './Components/SearchResults/SearchResults';
+
+const searchResults = [
+  {name: 'Song 1', artist: 'Singer 1', album: 'Album 1', uri: 'kdhwbvwi'},
+  {name: 'Song 2', artist: 'Singer 1', album: 'Album 1', uri: 'qvqevrvq'},
+  {name: 'Song 3', artist: 'Singer 2', album: 'Album 1', uri: 'qrvqeere'},
+  {name: 'Song 4', artist: 'Singer 3', album: 'Album 2', uri: 'qrvqdvre'},
+];
 
 function App() {
   return(
     <>
       <Header />
       <SearchBar />
+      <div>
+        <SearchResults searchResults={searchResults}/>
+      </div>
     </>
   );
 }
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tracklist from "../Tracklist/Tracklist";
+import './SearchResults.css';
+
+export default function SearchResults({searchResults}) {
+    return (
+        <div className="results">
+            <h2>Search Results</h2>
+            <Tracklist tracklist={searchResults}/>
+        </div>
+    );
+}
