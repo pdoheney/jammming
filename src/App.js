a6c8c850cda88288de1c79c709928c630ea7d924
Paul <pdoheney1@gmail.com>
2023-11-06 15:31:07 -0500

    Add a get accessToken method to Spotify module and added a SearchResults state to app component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ import SearchResults from './Components/SearchResults/SearchResults';
 
 import './App.css';
 
-const searchResults = [
-  {name: 'Song 1', artist: 'Singer 1', album: 'Album 1', uri: 'kdhwbvwi'},
-  {name: 'Song 2', artist: 'Singer 1', album: 'Album 1', uri: 'qvqevrvq'},
-  {name: 'Song 3', artist: 'Singer 2', album: 'Album 1', uri: 'qrvqeere'},
-  {name: 'Song 4', artist: 'Singer 3', album: 'Album 2', uri: 'qrvqdvre'},
-];
-
 function App() {
 
   //plus/minus symbols to differentiate between adding or removing a song
@@ -24,6 +17,8 @@ function App() {
   const [playlistTracks, setPlaylistTracks] = useState([]);
   //create state for storing playlist title
   const [playlistTitle, setplaylistTitle] = useState('');
+  //create state to store the search results from spotify API
+  const [searchResults, setSearchResults] = useState([]);
 
   //event handler that adds a song to playlist when user clicks the + button
   const addSong = track => {
---
+++ src/util/Spotify.js
@@ -0,0 +1,37 @@
+const clientId = '64ef146580e74b1d94f07df4322263ae';
+const redirectURL = 'http://localhost:3000/';
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        //if already have token from API merely return the token
+        if(accessToken) {
+            return accessToken;
+        }
+        //gather token data and expires in data from url using window.location.href which returns a string of the current url
+        //then use the match method that can be used on strings and regular expressions to find the access token and collect all chars 
+        //until get to an & symbol
+        //the expires in collects all digits after expires_in because it is end of the url string
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=(\d)*/);
+        //if both are truthy
+        if(accessTokenMatch && expiresInMatch) {
+            //match method returns an array where necessary info is second element
+            accessToken = accessTokenMatch[1];
+            //must convert string to number and convert seconds to milliseconds
+            const expiresIn = +expiresInMatch[1] * 1000;
+            //clear search parameters
+            window.pushState({}, null, '/');
+            //set timer to remove the accessToken when API expires per Spotify 
+            window.setTimeout(() => accessToken = '', expiresIn);
+            return accessToken;
+        } else {
+            //using implicit flow we set our url to this url the way spotify requires and then spotify will redirect back to
+            //web app with the access token and expires in in the url
+            const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=playlisy-modify-public&redirect_uri=${redirectURL}`;
+            window.location = url;
+        }
+    },
+};
+
+export default Spotify;
