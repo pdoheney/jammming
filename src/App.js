f259eae28a124d290c747377d4541dce4d62776d
Paul <pdoheney1@gmail.com>
2023-11-06 13:22:11 -0500

    Add Header component with basic styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
-  
+  return(
+    <Header />
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <h1>Ja<span>mmm</span>ing</h1>
+        </div>
+    );
+}
