e6ba2a9d0cabe432933dee3a8123d82a0f0d6bc9
Paul <pdoheney1@gmail.com>
2023-11-07 15:33:49 -0500

    Add try catch blocks to the create playlist method in spotify module.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -49,7 +49,6 @@ const Spotify = {
                 method: 'GET',
                 headers: { 'Authorization' : `Bearer ${accessToken}`}
             });
-            
             if(response.ok) {
                 const data = await response.json();
                 const tracklist = data.tracks.items.map(track => {
@@ -65,6 +64,7 @@ const Spotify = {
             throw new Error('Search Request Failed!');
         } catch(error) {
             console.log(error);
+            return [];
         }
     },
 
@@ -81,35 +81,65 @@ const Spotify = {
             name = 'New Playlist';
         }
 
+        //set id variables to be used in other requests
+        let userId;
+        let playlistId;
+
         //get userid from Spotify api
-        const userResponse = await fetch('https://api.spotify.com/v1/me', {
-            method: 'GET',
-            headers: { 'Authorization' : `Bearer ${accessToken}`}
-        });
-        const idResponse = await userResponse.json();
-        const userId = idResponse.id;
+        try {
+            const userResponse = await fetch('https://api.spotify.com/v1/me', {
+                method: 'GET',
+                headers: { 'Authorization' : `Bearer ${accessToken}`}
+            });
+            if(userResponse.ok) {
+                const idData = await userResponse.json();
+                userId = idData.id;
+            } else {
+                throw new Error('User information request failed!');
+            }
+        } catch(error) {
+            console.log(error);
+        }
 
         //use username to create a new playlist for that user 
-        const playlistResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-            method: 'POST',
-            headers: { 
-                'Authorization' : `Bearer ${accessToken}`,
-                'Content-Type' : 'application/json'
-            }, 
-            body: JSON.stringify({name: name})
-        });
-        const jsonResponse = await playlistResponse.json();
-        const playlistId = jsonResponse.id;
+        try {
+            const playlistResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+                method: 'POST',
+                headers: { 
+                    'Authorization' : `Bearer ${accessToken}`,
+                    'Content-Type' : 'application/json'
+                }, 
+                body: JSON.stringify({name: name})
+            });
+            if(playlistResponse.ok) {
+                const playlistData = await playlistResponse.json();
+                playlistId = playlistData.id;
+            } else {
+                throw new Error('Playlist Post request failed!');
+            }
+        } catch(error) {
+            console.log(error);
+        }
+        
 
         //add list of spotify uris to add tracks to playlist
-        await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({uris: uriList})
-        });
+        try {
+            const successResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({uris: uriList})
+            });
+            if(successResponse.ok) {
+                console.log('Success! Playlist created and songs have been added!');
+            } else {
+                throw new Error('Songs Post request failed!');
+            }
+        } catch(error) {
+            console.log(error);
+        }
     }
 };
 
