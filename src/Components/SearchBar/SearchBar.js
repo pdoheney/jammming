d3d3f333934f6f4367ea81db17f784ffeb3e4353
Paul <pdoheney1@gmail.com>
2023-11-08 09:29:18 -0500

    Adjust color to Header and improved styling of SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,8 +5,14 @@ import './SearchBar.css';
 export default function SearchBar({searchTerm, handleClick, handleChange}) {
     return (
         <form>
-            <input type="text" name="search" id="search" value={searchTerm} onChange={handleChange}/>
-            <button type="submit" onClick={handleClick}>Search</button>
+            <input 
+                type="text" 
+                name="search-input" 
+                id="search-input" 
+                value={searchTerm} 
+                onChange={handleChange} 
+                placeholder='Enter Song Title'/>
+            <button type="submit" id="search" onClick={handleClick}>Search</button>
         </form>
     );
 }
